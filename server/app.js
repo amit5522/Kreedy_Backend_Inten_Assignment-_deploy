e47fa831b9955484f3732b32887bf2f2cd10ef16
amit5511 <96247681+amit5511@users.noreply.github.com>
2023-02-27 13:41:22 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const app = express();
 const cors =require('cors');
-
+const path =require('path')
 
 //setting cors
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true,
 }));
 
-//configure dot env
-const dotenv = require("dotenv");
-dotenv.config({ path: "server/config/.env" });
+
 
 //middleware parses incoming requests with JSON
 app.use(express.json({limit: '50mb'}));
@@ -26,12 +24,11 @@ app.use('/api/v1',userRoute);
 
 
 
-//handeling req and res
-app.get('/',(req,res)=>{
-    res.status(200).json({
-     success:true,
-     msg:"Server is Working fine"
-    });
+//initalized frontend
+app.use(express.static(path.join(__dirname,'../client/build')));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'../client/build/index.html'))
 })
 
 
